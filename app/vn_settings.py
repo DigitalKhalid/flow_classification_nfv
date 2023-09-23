c9b8e2453fd2f6dfba6493b4791324c1aecbffe8
DigitalKhalid <waqas.ahmad.khalid@gmail.com>
2023-09-23 15:18:27 +0500

    Submission after 2nd review

--- app/vn_settings.py
+++ app/vn_settings.py
@@ -1,6 +1,10 @@
+# Parameters to customize virtual network simulation
 vn_hosts = 5                        # No. of hosts ranges from 2 ~ 25
 vn_duration = 1000                  # Duration in seconds
+
+# Parameters of generate packet injection file for virtual network simulation
 pkt_injection_type = 'sequential'   # sequential or random
-inj_flows_count = 100
-pkt_injection_time = 0.05           # This is a range starts from 0. Packets injected within this range at random interval.
+inj_flows_count = 100               # No. of flow generated
 injection_ratio = [10, 1]           # Ratio of Mice and Elephant Packets i.e [Mice, Elephant] = Mice : Elephant
+
+# pkt_injection_time = 0.05           # This is a range starts from 0. Packets injected within this range at random interval.
